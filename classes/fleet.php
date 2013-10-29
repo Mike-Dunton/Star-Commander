f8e8be456354eb3770460b671422510815e7d682
Michael Dunton <michael.dunton@patlive.com>
2013-10-28 20:41:41 -0400

    Added the ship class and the functions that go with it. Added Block comments to functions

--- classes/fleet.php
+++ classes/fleet.php
@@ -2,18 +2,35 @@
 
 class Fleet
 {
+    /**
+     * Holds the ID related to the entry in the database
+     * @var [int]
+     */
     private $FleetID;
+    /**
+     * Name of the Fleet
+     * @var [string]
+     */
     private $Name;
+    /**
+     * The amount of in game currancy held in this object
+     * @var [int]
+     */
     private $Credits;
 
+    /**
+     * Constructor takes a user ID and recreates a users fleet.
+     * @param [int] $id
+     */
     public function __construct($id)
     {
         loadByID($id);
     }
 
-    /*
-    *   Get the fleet data by userID
-    */
+    /**
+     * Loads the fleet given a user ID
+     * @param [int] $id
+     */
     private function loadByID( $id )
     {
         $dbh = dbHandler::getConnection();
@@ -22,48 +39,81 @@ class Fleet
         $this->fill( $row->fetch(PDO::FETCH_ASSOC) );
     }
 
-    /*
-    *   Add Credits to a fleets bank
+   /**
+    *  Add Credits to a fleets bank
+    * @param [int] $numCredits
     */
     public  function addToCredits($numCredits)
     {
-        $dbh = dbHandler::getConnection();
-
-        $update = $dbh->_dbh->prepare( 'UPDATE fleet
-                                        SET credits = credits + :numCredits
-                                        WHERE user_id = :id');
-        $update->execute(array("numCredits" => $numCredits,"id" => $FleetID));
-
-        //Update the Fleet information
-        this->loadByID($FleetID);
+        $this->Credits = $this->Credits + $numCredits;
     }
-
+    /**
+     * Set the name of the fleet
+     * @param [String] $newName
+     */
     public function setName($newName)
     {
-        $this->Name = $newName;	
+        $this->Name = $newName;
     }
+    /**
+     * Get the name of the fleet
+     * @return [string]
+     */
     public function getName()
     {
         return $this->Name;
     }
+    /**
+     * sets the number of credits
+     * @param [int] $newCredits
+     */
     public function setCredits($newCredits)
     {
-        $this->Credits = $newCredits; 
+        $this->Credits = $newCredits;
     }
+    /**
+     * Get the number of credits
+     * @return [int]
+     */
     public function getCredits()
     {
         return $this->Credits;
     }
 
-    /*
-    *   Does the actual filling of class data
-    */
+    /**
+     * Takes an array of data and sets the objects data.
+     * @param  [Array] $row
+     */
     private function fill( $row )
     {
         $this->FleetID = $row['fleet_id'];
         $this->Name = $row['name'];
         $this->Credits = $row['credits'];
     }
+    /**
+     * Get the objects data as an array
+     * @return [Array]
+     */
+    private function toArray()
+    {
+        return array("credits" => $this->Credits,
+                     "name" => $this->Name,
+                     "id" => $FleetID);
+    }
+    /**
+     * persist the object data to the database.
+     */
+    public function persist()
+    {
+        $dbh = dbHandler::getConnection();
 
+        $update = $dbh->_dbh->prepare( 'UPDATE fleet
+                                        SET credits = :credits, name = :name
+                                        WHERE user_id = :id');
+        $update->execute($this->toArray());
+
+        //Update the Fleet information
+        this->loadByID($this->FleetID);
+    }
 
 }
---
+++ classes/ship.php
@@ -0,0 +1,149 @@
+<?php
+
+class Ship
+{
+    /**
+     * Holds the ID related to the entry in the database
+     * @var [int]
+     */
+    private $shipID;
+
+    /**
+     * Holds the ID of the related fleet in the database
+     * @var [int]
+     */
+    private $fleetID;
+
+    /**
+     * Holds the ships class
+     * @var [int]
+     */
+    private $ClassID;
+
+    /**
+     * Holds the ID of what solar system the ship is in
+     * @var [type]
+     */
+    private $solarID;
+
+    /**
+     * Holds the current x coordinate
+     * @var [type]
+     */
+    private $coor_x;
+    /**
+     * @var [type]
+     */
+    private $coor_y;
+    /**
+     * Holds the name of the ship
+     * @var [string]
+     */
+    private $name;
+
+    /**
+     * Holds the dateTime stamp of the last action by the ship
+     * @var [dateTime]
+     */
+    private $dt_lastAction;
+
+    /**
+     * Holds the DateTime of the last ship movement
+     * @var [dataTime]
+     */
+    private $dt_lastMove;
+
+
+    /**
+     * Constructor takes a ship ID and recreates a ship
+     * @param [int] $id
+     */
+    public function __construct($id)
+    {
+        loadByID($id);
+    }
+
+    /**
+     * Loads the ship by ship ID
+     * @param [int] $id
+     */
+    private function loadByID( $id )
+    {
+        $dbh = dbHandler::getConnection();
+
+        $row = $dbh->_dbh->query('SELECT * FROM ship WHERE ship_id = ' .$id);
+        $this->fill( $row->fetch(PDO::FETCH_ASSOC) );
+    }
+
+    /**
+     * Set the name of the ship
+     * @param [String] $newName
+     */
+    public function setName($newName)
+    {
+        $this->nName = $newName;
+    }
+
+    /**
+     * Get the name of the ship
+     * @return [string]
+     */
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+     /**
+     * Takes an array of data and sets the objects data.
+     * @param  [Array] $row
+     */
+    private function fill( $row )
+    {
+        $this->shipID = $row['ship_id'];
+        $this->name = $row['name'];
+        $this->fleetID = $row['fleet_id'];
+        $this->classID = $row['class_id'];
+        $this->solarID = $row['solar_id'];
+        $this->coor_x = $row['coor_x'];
+        $this->coor_y = $row['coor_y'];
+        $this->dt_lastMove = $row['dt_lastMove'];
+        $this->dt_lastAction = $row['dt_lastAction'];
+    }
+    /**
+     * Get the objects data as an array
+     * @return [Array]
+     */
+    private function toArray()
+    {
+        return array("shipID" => $this->shipID,
+                     "name" => $this->name,
+                     "fleetID" => $fleetID,
+                     "classID" => $classID,
+                     "solarID" => $solarID,
+                     "coor_x" => $coor_x,
+                     "coor_y" => $coor_y,
+                     "dt_lastMove" => $dt_lastMove,
+                     "dt_lastAction" => $dt_lastAction);
+    }
+
+    /**
+     * persist the object data to the database.
+     */
+    public function persist()
+    {
+        $dbh = dbHandler::getConnection();
+
+        $update = $dbh->_dbh->prepare( 'UPDATE ship
+                                        SET name = :name,
+                                            solar_id = :solarID,
+                                            coor_x = :coor_x,
+                                            coor_y = :coor_y,
+                                            dt_lastMove = :dt_lastMove,
+                                            dt_lastAction = :dt_lastAction
+                                        WHERE ship_id = :shipID');
+        $update->execute($this->toArray());
+
+        this->loadByID($this->shipID);
+    }
+
+}
