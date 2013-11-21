ad611b0d2cf9a5bfde8f22b7f87c8150531c40a4
Michael Dunton <michael.dunton@patlive.com>
2013-11-21 16:37:33 -0500

    did some more work with galaxy Gen

--- includes/utils/makeSolarSystem.php
+++ includes/utils/makeSolarSystem.php
@@ -8,41 +8,56 @@ include_once('../../conn/db.php');
 // 4 - Astroid
 // 5 - Mystery_Object
 
-$galaxyArray = new array();
+$galaxyArray = array();
 
     function getItem() {
         $rN = mt_rand(1, 100); //random number between 1 and 100
-        if($rN >= 1 && $rN <= 40){
-            return 0 //empty space 40%
-        } elseif($rN >= 41 && $rN <= 55){
-            return 2 // planet 14%
-        } elseif($rN >= 56 && $rN <= 75) {
-            return 4 // Astroid 19%
-        } elseif($rN >= 76 && $rN <= 81) {
-            return 5 //Mystery 5%
-        } elseif ($rN >= 82 && $rN <= 100) {
-            return 3 // Station 18%
+        if($rN >= 1 && $rN <= 77){
+            return 0; //empty space 77%
+        } elseif($rN >= 78 && $rN <= 81){
+            return 2; // planet 3%
+        } elseif($rN >= 82 && $rN <= 90) {
+            return 4; // Astroid 8%
+        } elseif($rN >= 91 && $rN <= 92) {
+            return 5; //Mystery 1%
+        } elseif ($rN >= 92 && $rN <= 97) {
+            return 3; // Station 5%
+        }else {
+            return 0;
         }
     }
-}
 
-for(int x = 0; x<100; x++) {
-    for(int y = 0; y<100; y++) {
-        $galaxyArray[x][y] = 0;
+
+for($x=0; $x<60; $x++) {
+    for($y=0; $y<100; $y++) {
+        $galaxyArray[$x][$y] = 0;
     }
 }
 
-for(int x = 0; x<100; x++) {
-    for(int y = 0; y<100; y++) {
-        $galaxyArray[x][y] = getItem();
+for($x=0; $x<60; $x++) {
+    for($y=0; $y<100; $y++) {
+        usleep (300);
+        $galaxyArray[$x][$y] = getItem();
     }
 }
 
-print "| ";
-for(int x = 0; x<100; x++) {
-    for(int y = 0; y<100; y++) {
-        print " " . $galaxyArray[x][y] . " | ";
+?>
+<html>
+<body bgcolor="#000">
+<div style="width:1800px">
+<?
+for($x=0; $x<60; $x++) {
+    for($y=0; $y<100; $y++) {
+        if($galaxyArray[$x][$y] == 0){
+            echo "<span style='color:black; display:block; float:left'>" . $galaxyArray[$x][$y] ."</span>";
+        }else {
+            echo "<span style='color:white; display:block; float:left'>" . $galaxyArray[$x][$y]. "</span>";
+        }
+    
     }
-    print "\n";
+    print "<br style='clear:both>'";
 }
 ?>
+</div>
+</body>
+</html>
